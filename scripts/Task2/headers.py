44a5c1f642a6627f3c8b92c50a1fe197bf96dbf7
eyor23 <eyor2018@gmail.com>
2024-12-17 11:50:11 +0300

    Removed notebooks/analysisTask_2.ipynb and scripts/Tasks2 for transfer to task-2

--- scripts/Task2/headers.py
+++
@@ -1,9 +0,0 @@
-# header.py
-
-def notebook_header(title):
-    print("=" * 80)
-    print(f"Section Title: {title}")
-    print("=" * 80)
-    print("Author: Eyor Getachew")
-    print("Date: 2024-12-16")
-    print("=" * 80)
--- scripts/Task2/indicator.py
+++
@@ -1,9 +0,0 @@
-# indicators.py
-import talib
-import pandas as pd
-
-def calculate_indicators(stock_data):
-    stock_data['SMA_20'] = talib.SMA(stock_data['Close'], timeperiod=20)
-    stock_data['RSI'] = talib.RSI(stock_data['Close'], timeperiod=14)
-    stock_data['MACD'], stock_data['MACD_signal'], _ = talib.MACD(stock_data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-    return stock_data
